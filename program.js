fcf78e4775ca3f43334f6d2645350849a8a55eaa
blacksun1 <simon@blacksun.cx>
2016-11-22 06:33:37 +0000

    Meet pipe

--- program.js
+++ program.js
@@ -1,4 +1,11 @@
 "use strict";
 
-console.log("beep boop");
+const Assert = require("assert");
+const FS = require("fs");
 
+
+const filename = process.argv[2]
+Assert(filename, "Expects a filename");
+
+FS.createReadStream(filename)
+  .pipe(process.stdout)
