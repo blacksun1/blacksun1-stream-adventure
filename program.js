bce47605253af11cd34dbfd49a7d3d27ee96c441
blacksun1 <simon@blacksun.cx>
2016-11-22 09:45:24 +0000

    Concat

--- program.js
+++ program.js
@@ -1,80 +1,49 @@
 "use strict";
 
 /*
-Instead of transforming every line as in the previous "TRANSFORM" example,
-for this challenge, convert even-numbered lines to upper-case and odd-numbered
-lines to lower-case. Consider the first line to be odd-numbered. For example
-given this input:
+Concat
+======
 
-    One
-    Two
-    Three
-    Four
+You will be given text on process.stdin. Buffer the text and reverse it using
+the `concat-stream` module before writing it to stdout.
 
-Your program should output:
+`concat-stream` is a write stream that you can pass a callback to get the
+complete contents of a stream as a single buffer. Here's an example that uses
+concat to buffer POST content in order to JSON.parse() the submitted data:
 
-    one
-    TWO
-    three
-    FOUR
+    var concat = require('concat-stream');
+    var http = require('http');
 
-You can use the `split` module to split input by newlines. For example:
+    var server = http.createServer(function (req, res) {
+        if (req.method === 'POST') {
+            req.pipe(concat(function (body) {
+                var obj = JSON.parse(body);
+                res.end(Object.keys(obj).join('\n'));
+            }));
+        }
+        else res.end();
+    });
+    server.listen(5000);
 
-    var split = require('split');
-    process.stdin
-        .pipe(split())
-        .pipe(through2(function (line, _, next) {
-            console.dir(line.toString());
-            next();
-        }))
-    ;
+In your adventure you'll only need to buffer input with `concat()` from
+process.stdin.
 
-`split` will buffer chunks on newlines before you get them. In the previous
-example, we will get separate events for each line even though all the data
-probably arrives on the same chunk:
+Make sure to `npm install concat-stream` in the directory where your solution
+file is located.
 
-    $ echo -e 'one\ntwo\nthree' | node split.js
-    'one'
-    'two'
-    'three'
-
-Your own program should use `split` in this way, but you should transform the
-input and pipe the output through to `process.stdout`.
-
-Make sure to `npm install split through2` in the directory where your solution
-file lives.
 */
 
 const Assert = require("assert");
 const Through = require("through2");
 const Split = require("split");
+const Concat = require("concat-stream");
+const Reverse = require("reverse-string");
 
 
-function writeUpperCase(buffer, encoding, next) {
-
-  this.push(buffer.toString().toUpperCase());
-}
-
-function writeLowerCase(buffer, encoding, next) {
-
-  this.push(buffer.toString().toLowerCase());
-}
-
-const funcs = [writeUpperCase, writeLowerCase];
-
-function toggleUpperAndLower(buffer, encoding, next) {
-
-  this.lineNumber = this.lineNumber || 0
-  const funcIndex = (this.lineNumber += 1) % 2;
-  funcs[funcIndex].apply(this, arguments);
-  this.push("\n");
-
-  return next();
-}
-
-const upperCaseStream = Through(toggleUpperAndLower);
+const upperCaseStream = Through();
 
 process.stdin
-  .pipe(Split())
-  .pipe(upperCaseStream)
-  .pipe(process.stdout)
+  .pipe(Concat(function(body) {
+
+    console.log(Reverse(body.toString()));
+  }));
